5b1f700074e954b449f07ca40cea7bdedc90cf3d
Boris Staletic <boris.staletic@gmail.com>
2018-04-08 01:51:00 +0200

    unicode_test

--- ycmd/tests/clang/subcommands_test.py
+++ ycmd/tests/clang/subcommands_test.py
@@ -1363,7 +1363,7 @@ def Subcommands_GetDoc_Unicode_test( app ):
   pprint( response )
 
   eq_( response, {
-    'detailed_info': """\
+    'detailed_info': u"""\
 int member_with_å_unicøde
 This method has unicøde in it
 Type: int
