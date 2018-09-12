12cc3647a1956255b5bd40fece79dd8122672e5f
Boris Staletic <boris.staletic@gmail.com>
2018-09-12 12:29:09 +0200

    Simplify GoToInclude

--- ycmd/completers/cpp/clang_completer.py
+++ ycmd/completers/cpp/clang_completer.py
@@ -225,9 +225,9 @@ class ClangCompleter( Completer ):
 
 
   def _GoTo( self, request_data ):
-    include_response = self._ResponseForInclude( request_data )
-    if include_response:
-      return include_response
+    location = self._LocationForGoTo( 'GetIncludedFileLocation', request_data )
+    if location:
+      return _ResponseForLocation( location )
 
     location = self._LocationForGoTo( 'GetDefinitionOrDeclarationLocation',
                                       request_data )
@@ -237,9 +237,11 @@ class ClangCompleter( Completer ):
 
 
   def _GoToImprecise( self, request_data ):
-    include_response = self._ResponseForInclude( request_data )
-    if include_response:
-      return include_response
+    location = self._LocationForGoTo( 'GetIncludedFileLocation',
+                                      request_data,
+                                      reparse = False )
+    if location:
+      return _ResponseForLocation( location )
 
     location = self._LocationForGoTo( 'GetDefinitionOrDeclarationLocation',
                                       request_data,
@@ -249,40 +251,11 @@ class ClangCompleter( Completer ):
     return _ResponseForLocation( location )
 
 
-  def _ResponseForInclude( self, request_data ):
-    """Returns response for include file location if cursor is on the
-    include statement, None otherwise.
-    Throws RuntimeError if cursor is on include statement and corresponding
-    include file not found."""
-    current_line = request_data[ 'line_value' ]
-    include_file_name, quoted_include = GetFullIncludeValue( current_line )
-    if not include_file_name:
-      return None
-
-    flags, current_file_path = self._FlagsForRequest( request_data )
-    quoted_include_paths, include_paths = UserIncludePaths( flags,
-                                                            current_file_path )
-    if quoted_include:
-      include_file_path = _GetAbsolutePath( include_file_name,
-                                            quoted_include_paths )
-      if include_file_path:
-        return responses.BuildGoToResponse( include_file_path,
-                                            line_num = 1,
-                                            column_num = 1 )
-
-    include_file_path = _GetAbsolutePath( include_file_name, include_paths )
-    if include_file_path:
-      return responses.BuildGoToResponse( include_file_path,
-                                          line_num = 1,
-                                          column_num = 1 )
-    raise RuntimeError( 'Include file not found.' )
-
-
   def _GoToInclude( self, request_data ):
-    include_response = self._ResponseForInclude( request_data )
-    if not include_response:
-      raise RuntimeError( 'Not an include/import line.' )
-    return include_response
+    location = self._LocationForGoTo( 'GetIncludedFileLocation', request_data )
+    if not location or not location.IsValid():
+      raise RuntimeError( 'Include file not found.' )
+    return _ResponseForLocation( location )
 
 
   def _GetSemanticInfo(
