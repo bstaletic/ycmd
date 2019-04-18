0b5aa4686b7eba219404f616b82c969243fa6f33
Boris Staletic <boris.staletic@gmail.com>
2019-04-18 19:05:15 +0200

    test_3

--- ycmd/tests/java/server_management_test.py
+++ ycmd/tests/java/server_management_test.py
@@ -25,23 +25,15 @@ from builtins import *  # noqa
 
 import functools
 import os
-import psutil
-import requests
-import time
 
-from mock import patch
-from hamcrest import assert_that, contains, equal_to, has_entry
+from hamcrest import assert_that
 from ycmd.tests.java import ( PathToTestFile,
                               IsolatedYcmd,
-                              SharedYcmd,
                               StartJavaCompleterServerInDirectory )
 from ycmd.tests.test_utils import ( BuildRequest,
                                     CompleterProjectDirectoryMatcher,
-                                    ErrorMatcher,
-                                    MockProcessTerminationTimingOut,
-                                    TemporaryTestDir,
                                     WaitUntilCompleterServerReady )
-from ycmd import utils, handlers
+from ycmd import utils
 
 
 def TidyJDTProjectFiles( dir_name ):
@@ -270,245 +262,245 @@ def ServerManagement_OpenProject_RelativePath_test( app ):
 
 
 
-@SharedYcmd
-def ServerManagement_OpenProject_RelativePathNoWD_test( app ):
-  response = app.post_json(
-    '/run_completer_command',
-    BuildRequest(
-      filetype = 'java',
-      command_arguments = [
-        'OpenProject',
-        os.path.join( '..', 'simple_maven_project' ),
-      ],
-    ),
-    expect_errors = True,
-  )
-  assert_that( response.status_code,
-               equal_to( requests.codes.internal_server_error ) )
-  assert_that( response.json,
-               ErrorMatcher( ValueError,
-                             'Project directory must be absolute' ) )
-
-
-@SharedYcmd
-def ServerManagement_OpenProject_RelativePathNoPath_test( app ):
-  response = app.post_json(
-    '/run_completer_command',
-    BuildRequest(
-      filetype = 'java',
-      command_arguments = [
-        'OpenProject',
-      ],
-    ),
-    expect_errors = True,
-  )
-  assert_that( response.status_code,
-               equal_to( requests.codes.internal_server_error ) )
-  assert_that( response.json,
-               ErrorMatcher( ValueError,
-                             'Usage: OpenProject <project directory>' ) )
-
-
-def ServerManagement_ProjectDetection_NoParent_test():
-  with TemporaryTestDir() as tmp_dir:
-
-    @IsolatedYcmd()
-    def Test( app ):
-      StartJavaCompleterServerInDirectory( app, tmp_dir )
-
-      # Run the debug info to check that we have the correct project dir (cwd)
-      request_data = BuildRequest( filetype = 'java' )
-      assert_that( app.post_json( '/debug_info', request_data ).json,
-                   CompleterProjectDirectoryMatcher( tmp_dir ) )
-
-    yield Test
-
-
-@IsolatedYcmd()
-@patch( 'shutil.rmtree', side_effect = OSError )
-@patch( 'ycmd.utils.WaitUntilProcessIsTerminated',
-        MockProcessTerminationTimingOut )
-def ServerManagement_CloseServer_Unclean_test( app, *args ):
-  StartJavaCompleterServerInDirectory(
-    app, PathToTestFile( 'simple_eclipse_project' ) )
-
-  app.post_json(
-    '/run_completer_command',
-    BuildRequest(
-      filetype = 'java',
-      command_arguments = [ 'StopServer' ]
-    )
-  )
-
-  request_data = BuildRequest( filetype = 'java' )
-  assert_that( app.post_json( '/debug_info', request_data ).json,
-               has_entry(
-                 'completer',
-                 has_entry( 'servers', contains(
-                   has_entry( 'is_running', False )
-                 ) )
-               ) )
-
-
-@IsolatedYcmd()
-def ServerManagement_StopServerTwice_test( app ):
-  StartJavaCompleterServerInDirectory(
-    app, PathToTestFile( 'simple_eclipse_project' ) )
-
-  app.post_json(
-    '/run_completer_command',
-    BuildRequest(
-      filetype = 'java',
-      command_arguments = [ 'StopServer' ],
-    ),
-  )
-
-  request_data = BuildRequest( filetype = 'java' )
-  assert_that( app.post_json( '/debug_info', request_data ).json,
-               has_entry(
-                 'completer',
-                 has_entry( 'servers', contains(
-                   has_entry( 'is_running', False )
-                 ) )
-               ) )
-
-
-  # Stopping a stopped server is a no-op
-  app.post_json(
-    '/run_completer_command',
-    BuildRequest(
-      filetype = 'java',
-      command_arguments = [ 'StopServer' ],
-    ),
-  )
-
-  request_data = BuildRequest( filetype = 'java' )
-  assert_that( app.post_json( '/debug_info', request_data ).json,
-               has_entry(
-                 'completer',
-                 has_entry( 'servers', contains(
-                   has_entry( 'is_running', False )
-                 ) )
-               ) )
-
-
-@IsolatedYcmd()
-def ServerManagement_ServerDies_test( app ):
-  StartJavaCompleterServerInDirectory(
-    app,
-    PathToTestFile( 'simple_eclipse_project' ) )
-
-  request_data = BuildRequest( filetype = 'java' )
-  debug_info = app.post_json( '/debug_info', request_data ).json
-  print( 'Debug info: {0}'.format( debug_info ) )
-  pid = debug_info[ 'completer' ][ 'servers' ][ 0 ][ 'pid' ]
-  print( 'pid: {0}'.format( pid ) )
-  process = psutil.Process( pid )
-  process.terminate()
-
-  for tries in range( 0, 10 ):
-    request_data = BuildRequest( filetype = 'java' )
-    debug_info = app.post_json( '/debug_info', request_data ).json
-    if not debug_info[ 'completer' ][ 'servers' ][ 0 ][ 'is_running' ]:
-      break
-
-    time.sleep( 0.5 )
-
-  assert_that( debug_info,
-               has_entry(
-                 'completer',
-                 has_entry( 'servers', contains(
-                   has_entry( 'is_running', False )
-                 ) )
-               ) )
-
-
-@IsolatedYcmd()
-def ServerManagement_ServerDiesWhileShuttingDown_test( app ):
-  StartJavaCompleterServerInDirectory(
-    app,
-    PathToTestFile( 'simple_eclipse_project' ) )
-
-  request_data = BuildRequest( filetype = 'java' )
-  debug_info = app.post_json( '/debug_info', request_data ).json
-  print( 'Debug info: {0}'.format( debug_info ) )
-  pid = debug_info[ 'completer' ][ 'servers' ][ 0 ][ 'pid' ]
-  print( 'pid: {0}'.format( pid ) )
-  process = psutil.Process( pid )
-
-
-  def StopServerInAnotherThread():
-    app.post_json(
-      '/run_completer_command',
-      BuildRequest(
-        filetype = 'java',
-        command_arguments = [ 'StopServer' ],
-      ),
-    )
-
-  completer = handlers._server_state.GetFiletypeCompleter( [ 'java' ] )
-
+# @SharedYcmd
+# def ServerManagement_OpenProject_RelativePathNoWD_test( app ):
+#   response = app.post_json(
+#     '/run_completer_command',
+#     BuildRequest(
+#       filetype = 'java',
+#       command_arguments = [
+#         'OpenProject',
+#         os.path.join( '..', 'simple_maven_project' ),
+#       ],
+#     ),
+#     expect_errors = True,
+#   )
+#   assert_that( response.status_code,
+#                equal_to( requests.codes.internal_server_error ) )
+#   assert_that( response.json,
+#                ErrorMatcher( ValueError,
+#                              'Project directory must be absolute' ) )
+#
+#
+# @SharedYcmd
+# def ServerManagement_OpenProject_RelativePathNoPath_test( app ):
+#   response = app.post_json(
+#     '/run_completer_command',
+#     BuildRequest(
+#       filetype = 'java',
+#       command_arguments = [
+#         'OpenProject',
+#       ],
+#     ),
+#     expect_errors = True,
+#   )
+#   assert_that( response.status_code,
+#                equal_to( requests.codes.internal_server_error ) )
+#   assert_that( response.json,
+#                ErrorMatcher( ValueError,
+#                              'Usage: OpenProject <project directory>' ) )
+#
+#
+# def ServerManagement_ProjectDetection_NoParent_test():
+#   with TemporaryTestDir() as tmp_dir:
+#
+#     @IsolatedYcmd()
+#     def Test( app ):
+#       StartJavaCompleterServerInDirectory( app, tmp_dir )
+#
+#       # Run the debug info to check that we have the correct project dir (cwd)
+#       request_data = BuildRequest( filetype = 'java' )
+#       assert_that( app.post_json( '/debug_info', request_data ).json,
+#                    CompleterProjectDirectoryMatcher( tmp_dir ) )
+#
+#     yield Test
+#
+#
+# @IsolatedYcmd()
+# @patch( 'shutil.rmtree', side_effect = OSError )
+# @patch( 'ycmd.utils.WaitUntilProcessIsTerminated',
+#         MockProcessTerminationTimingOut )
+# def ServerManagement_CloseServer_Unclean_test( app, *args ):
+#   StartJavaCompleterServerInDirectory(
+#     app, PathToTestFile( 'simple_eclipse_project' ) )
+#
+#   app.post_json(
+#     '/run_completer_command',
+#     BuildRequest(
+#       filetype = 'java',
+#       command_arguments = [ 'StopServer' ]
+#     )
+#   )
+#
+#   request_data = BuildRequest( filetype = 'java' )
+#   assert_that( app.post_json( '/debug_info', request_data ).json,
+#                has_entry(
+#                  'completer',
+#                  has_entry( 'servers', contains(
+#                    has_entry( 'is_running', False )
+#                  ) )
+#                ) )
+#
+#
+# @IsolatedYcmd()
+# def ServerManagement_StopServerTwice_test( app ):
+#   StartJavaCompleterServerInDirectory(
+#     app, PathToTestFile( 'simple_eclipse_project' ) )
+#
+#   app.post_json(
+#     '/run_completer_command',
+#     BuildRequest(
+#       filetype = 'java',
+#       command_arguments = [ 'StopServer' ],
+#     ),
+#   )
+#
+#   request_data = BuildRequest( filetype = 'java' )
+#   assert_that( app.post_json( '/debug_info', request_data ).json,
+#                has_entry(
+#                  'completer',
+#                  has_entry( 'servers', contains(
+#                    has_entry( 'is_running', False )
+#                  ) )
+#                ) )
+#
+#
+#   # Stopping a stopped server is a no-op
+#   app.post_json(
+#     '/run_completer_command',
+#     BuildRequest(
+#       filetype = 'java',
+#       command_arguments = [ 'StopServer' ],
+#     ),
+#   )
+#
+#   request_data = BuildRequest( filetype = 'java' )
+#   assert_that( app.post_json( '/debug_info', request_data ).json,
+#                has_entry(
+#                  'completer',
+#                  has_entry( 'servers', contains(
+#                    has_entry( 'is_running', False )
+#                  ) )
+#                ) )
+#
+#
+# @IsolatedYcmd()
+# def ServerManagement_ServerDies_test( app ):
+#   StartJavaCompleterServerInDirectory(
+#     app,
+#     PathToTestFile( 'simple_eclipse_project' ) )
+#
+#   request_data = BuildRequest( filetype = 'java' )
+#   debug_info = app.post_json( '/debug_info', request_data ).json
+#   print( 'Debug info: {0}'.format( debug_info ) )
+#   pid = debug_info[ 'completer' ][ 'servers' ][ 0 ][ 'pid' ]
+#   print( 'pid: {0}'.format( pid ) )
+#   process = psutil.Process( pid )
+#   process.terminate()
+#
+#   for tries in range( 0, 10 ):
+#     request_data = BuildRequest( filetype = 'java' )
+#     debug_info = app.post_json( '/debug_info', request_data ).json
+#     if not debug_info[ 'completer' ][ 'servers' ][ 0 ][ 'is_running' ]:
+#       break
+#
+#     time.sleep( 0.5 )
+#
+#   assert_that( debug_info,
+#                has_entry(
+#                  'completer',
+#                  has_entry( 'servers', contains(
+#                    has_entry( 'is_running', False )
+#                  ) )
+#                ) )
+#
+#
+# @IsolatedYcmd()
+# def ServerManagement_ServerDiesWhileShuttingDown_test( app ):
+#   StartJavaCompleterServerInDirectory(
+#     app,
+#     PathToTestFile( 'simple_eclipse_project' ) )
+#
+#   request_data = BuildRequest( filetype = 'java' )
+#   debug_info = app.post_json( '/debug_info', request_data ).json
+#   print( 'Debug info: {0}'.format( debug_info ) )
+#   pid = debug_info[ 'completer' ][ 'servers' ][ 0 ][ 'pid' ]
+#   print( 'pid: {0}'.format( pid ) )
+#   process = psutil.Process( pid )
+#
+#
+#   def StopServerInAnotherThread():
+#     app.post_json(
+#       '/run_completer_command',
+#       BuildRequest(
+#         filetype = 'java',
+#         command_arguments = [ 'StopServer' ],
+#       ),
+#     )
+#
+#   completer = handlers._server_state.GetFiletypeCompleter( [ 'java' ] )
+#
   # In this test we mock out the sending method so that we don't actually send
   # the shutdown request. We then assisted-suicide the downstream server, which
   # causes the shutdown request to be aborted. This is interpreted by the
   # shutdown code as a successful shutdown. We need to do the shutdown and
   # terminate in parallel as the post_json is a blocking call.
-  with patch.object( completer.GetConnection(), 'WriteData' ):
-    stop_server_task = utils.StartThread( StopServerInAnotherThread )
-    process.terminate()
-    stop_server_task.join()
-
-  request_data = BuildRequest( filetype = 'java' )
-  debug_info = app.post_json( '/debug_info', request_data ).json
-  assert_that( debug_info,
-               has_entry(
-                 'completer',
-                 has_entry( 'servers', contains(
-                   has_entry( 'is_running', False )
-                 ) )
-               ) )
-
-
-@IsolatedYcmd()
-def ServerManagement_ConnectionRaisesWhileShuttingDown_test( app ):
-  StartJavaCompleterServerInDirectory(
-    app,
-    PathToTestFile( 'simple_eclipse_project' ) )
-
-  request_data = BuildRequest( filetype = 'java' )
-  debug_info = app.post_json( '/debug_info', request_data ).json
-  print( 'Debug info: {0}'.format( debug_info ) )
-  pid = debug_info[ 'completer' ][ 'servers' ][ 0 ][ 'pid' ]
-  print( 'pid: {0}'.format( pid ) )
-  process = psutil.Process( pid )
-
-  completer = handlers._server_state.GetFiletypeCompleter( [ 'java' ] )
-
+#   with patch.object( completer.GetConnection(), 'WriteData' ):
+#     stop_server_task = utils.StartThread( StopServerInAnotherThread )
+#     process.terminate()
+#     stop_server_task.join()
+#
+#   request_data = BuildRequest( filetype = 'java' )
+#   debug_info = app.post_json( '/debug_info', request_data ).json
+#   assert_that( debug_info,
+#                has_entry(
+#                  'completer',
+#                  has_entry( 'servers', contains(
+#                    has_entry( 'is_running', False )
+#                  ) )
+#                ) )
+#
+#
+# @IsolatedYcmd()
+# def ServerManagement_ConnectionRaisesWhileShuttingDown_test( app ):
+#   StartJavaCompleterServerInDirectory(
+#     app,
+#     PathToTestFile( 'simple_eclipse_project' ) )
+#
+#   request_data = BuildRequest( filetype = 'java' )
+#   debug_info = app.post_json( '/debug_info', request_data ).json
+#   print( 'Debug info: {0}'.format( debug_info ) )
+#   pid = debug_info[ 'completer' ][ 'servers' ][ 0 ][ 'pid' ]
+#   print( 'pid: {0}'.format( pid ) )
+#   process = psutil.Process( pid )
+#
+#   completer = handlers._server_state.GetFiletypeCompleter( [ 'java' ] )
+#
   # In this test we mock out the GetResponse method, which is used to send the
   # shutdown request. This means we only send the exit notification. It's
   # possible that the server won't like this, but it seems reasonable for it to
   # actually exit at that point.
-  with patch.object( completer.GetConnection(),
-                     'GetResponse',
-                     side_effect = RuntimeError ):
-    app.post_json(
-      '/run_completer_command',
-      BuildRequest(
-        filetype = 'java',
-        command_arguments = [ 'StopServer' ],
-      ),
-    )
-
-  request_data = BuildRequest( filetype = 'java' )
-  debug_info = app.post_json( '/debug_info', request_data ).json
-  assert_that( debug_info,
-               has_entry(
-                 'completer',
-                 has_entry( 'servers', contains(
-                   has_entry( 'is_running', False )
-                 ) )
-               ) )
-
-  if process.is_running():
-    process.terminate()
-    raise AssertionError( 'jst.ls process is still running after exit handler' )
+#   with patch.object( completer.GetConnection(),
+#                      'GetResponse',
+#                      side_effect = RuntimeError ):
+#     app.post_json(
+#       '/run_completer_command',
+#       BuildRequest(
+#         filetype = 'java',
+#         command_arguments = [ 'StopServer' ],
+#       ),
+#     )
+#
+#   request_data = BuildRequest( filetype = 'java' )
+#   debug_info = app.post_json( '/debug_info', request_data ).json
+#   assert_that( debug_info,
+#                has_entry(
+#                  'completer',
+#                  has_entry( 'servers', contains(
+#                    has_entry( 'is_running', False )
+#                  ) )
+#                ) )
+#
+#   if process.is_running():
+#     process.terminate()
+  # raise AssertionError( 'jst.ls process is still running after exit handler' )
