c6f60c9e05af60e402b5c8a2ddf84bbb89c95864
Boris Staletic <boris.staletic@gmail.com>
2019-09-17 12:25:40 +0200

    s

--- ycmd/tests/java/subcommands_test.py
+++ ycmd/tests/java/subcommands_test.py
@@ -130,7 +130,7 @@ def RunTest( app, test, contents = None ):
   if not contents:
     contents = ReadFile( test[ 'request' ][ 'filepath' ] )
     print( '================================' )
-    print( 'contens = {}'.format( contents ) )
+    repr( 'contens = {}'.format( contents ) )
     print( '================================' )
 
   # Because we aren't testing this command, we *always* ignore errors. This
