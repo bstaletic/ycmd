f83e786c25e34646e51944b8b3b49b9c39c3db16
Boris Staletic <boris.staletic@gmail.com>
2020-01-22 23:41:17 +0100

    azure msvc oom

--- azure/windows/benchmark.bat
+++ azure/windows/benchmark.bat
@@ -3,4 +3,4 @@
 
 :: python benchmark.py
 
-cl.exe /c /I cpp\ctre /I cpp\robin_hood /std:c++17 /DYCM_EXPORT="__declspec( dllexport )" cpp\ycm\IdentifierUtils.cpp
+C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Tools/MSVC/14.24.28314/bin/Hostx64/x64/cl.exe /c /I cpp\ctre /I cpp\robin_hood /std:c++17 /DYCM_EXPORT="__declspec( dllexport )" cpp\ycm\IdentifierUtils.cpp
