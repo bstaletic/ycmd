19c5ba256449ec0b194150ab5eac4f6b1293b4e7
Boris Staletic <boris.staletic@gmail.com>
2020-01-22 23:30:48 +0100

    azure msvc oom

--- azure/windows/benchmark.bat
+++ azure/windows/benchmark.bat
@@ -1,4 +1,6 @@
 :: Add Python to PATH
-set "PATH=C:\Python;C:\Python\Scripts;%PATH%"
+:: set "PATH=C:\Python;C:\Python\Scripts;%PATH%"
 
-python benchmark.py
+:: python benchmark.py
+
+cl.exe /c /I cpp\ctre /I cpp\robin_hood /std:c++17 /DYCM_EXPORT="__declspec( dllexport )" cpp\ycm\IdentifierUtils.cpp
