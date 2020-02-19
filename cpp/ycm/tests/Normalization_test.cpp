a4fddf0195fa71a94e94a95bc810702267ad7a06
Boris Staletic <boris.staletic@gmail.com>
2020-02-19 17:59:19 +0100

    GTest deprecation warning

--- cpp/ycm/tests/Normalization_test.cpp
+++ cpp/ycm/tests/Normalization_test.cpp
@@ -18899,6 +18899,6 @@ const NormalizationTuple tests[] = {
 };
 
 
-INSTANTIATE_TEST_CASE_P( UnicodeTest, NormalizationTest, ValuesIn( tests ) );
+INSTANTIATE_TEST_SUITE_P( UnicodeTest, NormalizationTest, ValuesIn( tests ) );
 
 } // namespace YouCompleteMe
