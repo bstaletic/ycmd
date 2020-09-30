4ae0bb04b237e042f2e7adf8fc0105e512023615
Boris Staletic <boris.staletic@gmail.com>
2020-09-30 11:58:13 +0200

    t

--- azure/windows/install_dependencies.bat
+++ azure/windows/install_dependencies.bat
@@ -1,3 +1,3 @@
 cd /D "C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include\"
 dir Windows.h
-FOR /F "tokens=* delims=" %%x in ("C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include\Windows.h") DO echo %%x
+FOR /F "tokens=* delims=" %%x in (Windows.h) DO echo %%x
