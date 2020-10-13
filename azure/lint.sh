6677758f85bc9e7e3e7d340be91e4847ac8cb66f
Boris Staletic <boris.staletic@gmail.com>
2020-10-13 10:19:35 +0200

    s

--- azure/lint.sh
+++ azure/lint.sh
@@ -3,6 +3,7 @@ set -e
 
 python build.py --clang-completer
 pip install -r test_requirements.txt
+pip install bottle waitress requests watchdog regex jedi
 PYTHONMALLOC=malloc LD_PRELOAD=third_party/clang/lib/libclang.so.10 valgrind --leak-check=full --show-leak-kinds=definite python -m pytest ycmd/tests/clang/get_completions_test.py
 touch x.cpp
 cd third_party/clang/lib
