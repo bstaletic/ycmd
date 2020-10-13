7b7a356aa9ab3ab5cb0465840b3eaab2d7cf798b
Boris Staletic <boris.staletic@gmail.com>
2020-10-13 22:48:23 +0200

    test

--- azure/lint.sh
+++ azure/lint.sh
@@ -12,7 +12,7 @@ pyenv global ${YCM_PYTHON_VERSION}
 python_version=$(python -c 'import sys; print( "{}.{}.{}".format( *sys.version_info[:3] ) )')
 echo "Checking python version (actual ${python_version} vs expected ${YCM_PYTHON_VERSION})"
 test ${python_version} == ${YCM_PYTHON_VERSION}
-YCM_TESTRUN=1 python build.py --clang-completer --clang-tidy --valgrind
-python run_tests.py --valgrind --skip-build --no-flake8
+python build.py --clang-complete
+PYTHONMALLOC=malloc LD_LIBRARY_PATH=third_party/clang/lib valgrind --error-exitcode=1 python -m pytest ycmd/tests/clang/diagnostics_test.py
 
 set +e
