78068f985e721bfd1b7243da23bbdcd4c1a45762
Boris Staletic <boris.staletic@gmail.com>
2020-10-14 08:19:20 +0200

    test

--- azure/lint.sh
+++ azure/lint.sh
@@ -1,17 +1,36 @@
 # Exit immediately if a command returns a non-zero status.
 set -e
+sudo apt-get update
+sudo apt-get install libsqlite3-dev
+sudo apt-get install -y valgrind
 
 test -d "$HOME/.pyenv/bin" && export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 
-pyenv global ${YCM_PYTHON_VERSION}
+mkdir ${HOME}/.cache
 
-# It is quite easy to get the steps to configure Python wrong. Verify that the
-# version of Python actually in the PATH and used is the version that was
-# requested, and fail the build if we broke the setup.
-python_version=$(python -c 'import sys; print( "{}.{}.{}".format( *sys.version_info[:3] ) )')
-echo "Checking python version (actual ${python_version} vs expected ${YCM_PYTHON_VERSION})"
-test ${python_version} == ${YCM_PYTHON_VERSION}
+sudo apt-get install -y build-essential \
+                        libssl-dev \
+                        zlib1g-dev \
+                        libbz2-dev \
+                        libreadline-dev \
+                        libsqlite3-dev \
+                        wget \
+                        curl \
+                        llvm \
+                        libncurses5-dev \
+                        libncursesw5-dev \
+                        xz-utils \
+                        tk-dev \
+                        libffi-dev \
+                        liblzma-dev \
+                        python-openssl \
+                        git
+curl https://pyenv.run | bash
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install ${YCM_PYTHON_VERSION}
+pyenv global ${YCM_PYTHON_VERSION}
 pip install pytest
 python run_tests.py --valgrind
 
--- tests/clang/diagnostics_test.py
+++ tests/clang/diagnostics_test.py
@@ -16,17 +16,10 @@
 # along with ycmd.  If not, see <http://www.gnu.org/licenses/>.
 
 import pytest
-shared_app = None
 
+@pytest.fixture
+def f():
+  pass
 
-def SetUpApp( custom_options = {} ):
-  return 1
-
-
-@pytest.fixture( scope='module', autouse=True )
-def set_up_shared_app():
-  global shared_app
-  shared_app = SetUpApp( { 'use_clangd': 0 } )
-
-def Diagnostics_ZeroBasedLineAndColumn_test():
+def test_test( f ):
   return
