a079aed1f602798af9c584c5acb0d36a0e4ecde8
Boris Staletic <boris.staletic@gmail.com>
2021-04-19 20:04:07 +0200

    Fix codecov reports

--- azure/send_coverage.sh
+++ azure/send_coverage.sh
@@ -1,11 +1,6 @@
 # Exit immediately if a command returns a non-zero status.
 set -e
 
-test -d "$HOME/.pyenv/bin" && export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init -)"
-
-pyenv global ${YCM_PYTHON_VERSION}
-
-codecov --name "${CODECOV_JOB_NAME}"
+bash <(curl -s https://codecov.io/bash)
 
 set +e
